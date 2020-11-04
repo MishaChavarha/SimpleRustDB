2b3893763087db4f7eb8d5a5c358351930f29527
MishaChavarha <mishachavarha@gmail.com>
2020-11-04 14:43:08 +0200

    removed target

--- target/debug/deps/SimpleRustDB1-ffe1724c32ac5acc.d
+++
@@ -1,5 +0,0 @@
-/home/misha/Projects/SimpleRustDB/target/debug/deps/SimpleRustDB1-ffe1724c32ac5acc.rmeta: src/main.rs
-
-/home/misha/Projects/SimpleRustDB/target/debug/deps/SimpleRustDB1-ffe1724c32ac5acc.d: src/main.rs
-
-src/main.rs:
